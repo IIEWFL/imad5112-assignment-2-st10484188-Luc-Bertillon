d9b449434cece1ade28621f086928d2071f8c981
st10484188-Luc-Bertillon <st10484188@vcconnect.edu.za>
2025-05-20 21:19:28 +0200

    coded the Exit button for score page

--- app/src/main/java/com/example/quiz/Score.kt
+++ app/src/main/java/com/example/quiz/Score.kt
@@ -2,13 +2,19 @@ package com.example.quiz
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
 
 class Score : AppCompatActivity() {
+
+    private var counter = 0
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,18 @@ class Score : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnexit = findViewById<Button>(R.id.btnExit)
+
+        btnexit.setOnClickListener {
+            val toast = Toast.makeText(this, "Click again to exit", Toast.LENGTH_SHORT)
+            toast.show()
+            counter += 1
+
+            if (counter == 2) {
+                finishAffinity()
+            }
         }
     }
 }
