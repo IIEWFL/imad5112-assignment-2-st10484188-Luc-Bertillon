4f0d74abdde06dcf2e34442209cc74d8fb87705b
st10484188-Luc-Bertillon <st10484188@vcconnect.edu.za>
2025-05-20 20:47:57 +0200

    Created score page

--- app/src/main/java/com/example/quiz/Score.kt
+++ app/src/main/java/com/example/quiz/Score.kt
@@ -1,12 +1,15 @@
 package com.example.quiz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Score : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
